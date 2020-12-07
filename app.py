f910aa0d0938ef944e2f661683d1812c0ae1c1e4
hujiacheng <h1300704118@gmail.com>
2020-12-07 18:02:56 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from concurrent.futures import thread
+
+from flask import Flask, render_template
+import  detector
+
+app = Flask(__name__)  # 绑定app
+
+
+# 函数一：关联页面
+@app.route('/')  # 路由
+def image():
+    return render_template('image.html')
+
+
+
+# 函数二：生成数据
+from flask import jsonify  # 数据转为json，并以字典的形式传回前端
+import datetime, random  # 导入时间和随机数模块
+
+
+@app.route('/setData/')  # 路由
+def setData():
+    while True:
+        print("hello world")
+        now = datetime.datetime.now().strftime('%H:%M:%S')
+        array = detector.detector()
+        height = 0
+        temperature = 0
+        a = array[0]
+        b = array[1]
+        if a[-2:-1] == 'm':
+            height = float(a[:-2])
+            temperature=float(b[:-2])
+        else:
+            height = float(b[:-2])
+
+            temperature = float(a[:-2])
+
+        data = {'time':now, 'h':height,'t':temperature}
+        #data = {'time': now, 'data': random.randint(1, 10)}
+        return jsonify(data)  # 将数据以字典的形式传回
+
+# @app.route('/setData1/')
+# def setData1():
+#     while True:
+#         print("hello world")
+#         now = datetime.datetime.now().strftime('%H:%M:%S')
+#         height = detector.detector()
+#         data = {'time':now, 'data':height}
+#         #data = {'time': now, 'data': random.randint(1, 10)}
+#         return jsonify(data)  # 将数据以字典的形式传回
+
+# @app.route('/setData2/')
+# def setDataTemperature():
+#     while True:
+#         print("hello world")
+#         now = datetime.datetime.now().strftime('%H:%M:%S')
+#         temperature = detector.temperatureDetecte()
+#         data = {'time':now, 'data':temperature}
+#         #data = {'time': now, 'data': random.randint(1, 10)}
+#         return jsonify(data)  # 将数据以字典的形式传回
+
+if __name__ == '__main__':
+    app.run()
---
+++ detector.py
@@ -0,0 +1,89 @@
+# TODO 串口读取数据
+# Auther wjw
+
+import serial  # 导入串口包
+import time  # 导入时间包
+
+ser = serial.Serial("COM3", 115200, timeout=5)  # 开启com3口，波特率115200，超时5
+ser.flushInput()  # 清空缓冲区
+
+
+def detector():
+    detectorArray = []
+    while True:
+        count = ser.inWaiting()  # 获取串口缓冲区数据
+        if count != 0:
+            recv = ser.read(ser.in_waiting)  # 读出串口数据，数据采用gbk编码
+            a = bytearray(recv)
+            if len(a) == 13:
+                b = str(hex(a[11]))[2:]
+                c = str(hex(a[12]))[2:]
+                d = b + c
+                height = int(d, 16) * 2 / 100
+                print(height, end=' ')
+                print('cm')
+                detectorArray.append(str(height)+"cm")
+            if len(a) == 14:
+                b = str(hex(a[11]))  # 温度
+                temperature = int(b, 16)
+                print(temperature, end=' ')
+                detectorArray.append(str(temperature)+"'c")
+        if len(detectorArray)==2 :
+            return detectorArray
+            # b = recv[:-1]
+            # print(int.from_bytes(recv, byteorder='big', signed=False))
+            # print(time.time()," ---  recv --> ", recv)  # 打印一下子
+    time.sleep(0.1)  # 延时0.1秒，免得CPU出问题
+
+
+# def detector1():
+#     while True:
+#         count = ser.inWaiting()  # 获取串口缓冲区数据
+#         if count != 0:
+#             recv = ser.read(ser.in_waiting)  # 读出串口数据，数据采用gbk编码
+#             a = bytearray(recv)
+#             if len(a) == 13:
+#                 b = str(hex(a[11]))[2:]
+#                 c = str(hex(a[12]))[2:]
+#                 d = b + c
+#                 height = int(d, 16) * 2 / 100
+#                 print(height, end=' ')
+#                 print('cm')
+#                 return height
+#             if len(a) == 14:
+#                 b = str(hex(a[11]))  # 温度
+#                 temperature = int(b, 16)
+#                 print(temperature, end=' ')
+#                 return temperature
+#             # b = recv[:-1]
+#             # print(int.from_bytes(recv, byteorder='big', signed=False))
+#             # print(time.time()," ---  recv --> ", recv)  # 打印一下子
+#         time.sleep(0.1)  # 延时0.1秒，免得CPU出问题
+
+
+
+# def temperatureDetecte():
+#     while True:
+#         count = ser.inWaiting()  # 获取串口缓冲区数据
+#         if count != 0:
+#             recv = ser.read(ser.in_waiting)  # 读出串口数据，数据采用gbk编码
+#             a = bytearray(recv)
+#             if len(a) == 14:
+#                 b = str(hex(a[11]))  # 温度
+#                 # c = str(hex(a[12]))[2:]
+#                 temperature = int(b, 16)
+#                 print(temperature, end=' ')
+#                 print('‘C')
+#
+#                 #return temperature
+#
+#             # b = recv[:-1]
+#             # print(int.from_bytes(recv, byteorder='big', signed=False))
+#             # print(time.time()," ---  recv --> ", recv)  # 打印一下子
+#         time.sleep(0.1)  # 延时0.1秒，免得CPU出问题
+
+
+if __name__ == '__main__':
+    m = detector()
+    print(m[0][:-2])
+    print(m[1][:-2])
